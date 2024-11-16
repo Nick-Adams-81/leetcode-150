e315cfb4e02c83d9cb137b78684eefb36e906a93
Nick Adams <nick.pappas.adams@gmail.com>
2024-11-16 12:06:37 -0800

    Work in progress

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import ArraysAndHashing.ContainsDuplicate;
 import ArraysAndHashing.IsAnagram;
+import ArraysAndHashing.TwoSum;
+
+import java.util.Arrays;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -7,10 +10,12 @@ public class Main {
     public static void main(String[] args) {
         ContainsDuplicate duplicate = new ContainsDuplicate();
         IsAnagram anagram = new IsAnagram();
+        TwoSum twoSum = new TwoSum();
         int[] nums = {1,2,3,4};
 
         System.out.println(duplicate.hasDuplicate(nums));
         System.out.println(anagram.anagram("olleh", "hello"));
+        System.out.println(Arrays.toString(twoSum.twoSum(nums, 4)));
 
     }
 }
