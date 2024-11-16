fe395db08347709a6ea8dba5204ea2c23fdf9dc2
Nick Adams <nick.pappas.adams@gmail.com>
2024-11-16 15:03:47 -0800

    Work in progress

---
+++ src/ArraysAndHashing/ProductOfArrayExceptSelf.java
@@ -0,0 +1,23 @@
+package ArraysAndHashing;
+
+public class ProductOfArrayExceptSelf {
+
+    public int[] products(int[] nums) {
+        int n = nums.length;
+        int[] leftProducts = new int[n];
+        int[] rightProducts = new int[n];
+        int[] result = new int[n];
+        leftProducts[0] = 1;
+        rightProducts[n - 1] = 1;
+        for (int i = 1; i < n; i++) {
+            leftProducts[i] = nums[i -1] * leftProducts[i - 1];
+        }
+        for(int i = n - 2; i >= 0; i--) {
+            rightProducts[i] = nums[i + 1] * rightProducts[i + 1];
+        }
+        for(int i = 0; i < n; i++) {
+            result[i] = leftProducts[i] * rightProducts[i];
+        }
+        return result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
-import ArraysAndHashing.ContainsDuplicate;
-import ArraysAndHashing.GroupAnagrams;
-import ArraysAndHashing.IsAnagram;
-import ArraysAndHashing.TwoSum;
+import ArraysAndHashing.*;
 
 import java.util.Arrays;
 
@@ -13,14 +10,20 @@ public class Main {
         IsAnagram anagram = new IsAnagram();
         TwoSum twoSum = new TwoSum();
         GroupAnagrams groupAnagrams = new GroupAnagrams();
+        TopKFrequentElement el = new TopKFrequentElement();
+        ProductOfArrayExceptSelf products = new ProductOfArrayExceptSelf();
 
         int[] nums = {1,2,3,4};
+        int[] nums2 = {1, 2, 2, 3, 3, 3, 3};
+        int[] nums3 = {1, 2, 4, 6};
         String[] strings = {"act", "pots", "tops", "cat", "stop", "hat"};
 
         System.out.println(duplicate.hasDuplicate(nums));
         System.out.println(anagram.anagram("olleh", "hello"));
         System.out.println(Arrays.toString(twoSum.twoSum(nums, 4)));
         System.out.println(groupAnagrams.anagrams(strings));
+        System.out.println(Arrays.toString(el.kthElement(nums2, 2)));
+        System.out.println(Arrays.toString(products.products(nums3)));
 
     }
 }
